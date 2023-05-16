be67751049a738a9289833cc7a2351efae74c478
Yehan20 <yehansk15@gmail.com>
2023-05-17 03:50:43 +0530

    .

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
      origin:'http://127.0.0.1:5173',
      credentials:true,
-     allowedHeaders:true
+    
     
 }));
 app.use(session({
